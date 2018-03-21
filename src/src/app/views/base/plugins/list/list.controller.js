3d87834b34144f955e265156c1297be22e955fb7
Rafael Reis <reisraff@gmail.com>
2018-03-21 13:11:08 -0300

    fixing some stuff

--- src/src/app/views/base/plugins/list/list.controller.js
+++ src/src/app/views/base/plugins/list/list.controller.js
@@ -52,6 +52,21 @@
           });
         }
       };
+
+      _self.clicked = function (hash) {
+        scroll.scrollTo(scroll.getPosition(hash) - document.getElementById('menuScroll-element').offsetHeight, 0.2);
+        angular.element(document.querySelector('.cd-secondary-nav-trigger')).removeClass('menu-is-open');
+        angular.forEach(document.getElementById('menuScroll-element').querySelectorAll('ul'), function (value) {
+          angular.element(value).removeClass('is-visible');
+        });
+      };
+
+      _self.menu = function() {
+        angular.element(document.querySelector('.cd-secondary-nav-trigger')).toggleClass('menu-is-open');
+        angular.forEach(document.getElementById('menuScroll-element').querySelectorAll('ul'), function (value) {
+          angular.element(value).toggleClass('is-visible');
+        });
+      };
     }
   );
 
